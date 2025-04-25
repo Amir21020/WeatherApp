73d30eb20e8ebb41d1d862fa673dad48e7abb98d
Amir Amirov <mergalievradmir484@gmail.com>
2025-04-25 20:46:24 +0300

    feat: add weather footer component for weather app

---
+++ WeatherApp/src/components/WeatherFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bottom-text" style="margin: 5%;">
+      <div class="d-flex align-items-start ms-5">
+        <p class="me-3" style="font-size: 8rem; line-height: 1;">16°</p>
+        <div class="d-flex flex-column align-items-start" style="line-height: 1.45;">
+          <p class="mb-0" style="font-size: 4rem;">London</p>
+          <p class="mb-0" style="font-size: 1.15rem;">06:09 - Monday, 9 Sep '23</p>
+        </div>
+        <img src="/cloudy.svg" alt="Cloudy" class="ms-3 align-self-start" style="width: 80px; margin-top: 2.3%;">
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+  .bottom-text {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 1rem;
+  }
+  </style>
