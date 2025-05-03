58567298b7f16a9186a488c7ee22e4f8dde8dac8
Amir Amirov <mergalievradmir484@gmail.com>
2025-05-03 21:25:43 +0300

    "security: patch Vite configuration vulnerabilities"

--- WeatherApp/vite.config.js
+++ WeatherApp/vite.config.js
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/WeatherApp/',
+  server: {
+    fs: {
+      strict: true,
+      deny: ['.env', '*.pem'], 
+      allow: ['src'] 
+    }
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
